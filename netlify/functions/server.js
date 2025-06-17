3bd74b6fe83b7a3c2e5e70c9ad3a6284169bb862
Your Name <hoanganhdo182@gmail.com>
2025-06-18 02:16:42 +0700

    hh

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const app = require('./server/app');
+const path = require('path');
 
+// Tạo một ứng dụng Express mới
+const app = express();
+const router = express.Router();
+
+// Lấy app từ file app.js
+const mainApp = require('./server/app');
+
+// Gắn app chính vào router
+router.use(mainApp);
+
+// Thiết lập static files
+const staticPath = path.join(__dirname, 'server', 'public');
+app.use(express.static(staticPath));
+
+// Sử dụng router cho function
+app.use('/.netlify/functions/server', router);
+
+// Xuất handler
 module.exports.handler = serverless(app); 
