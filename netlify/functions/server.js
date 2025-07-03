a8ae03978acdfc55eb62c42f5f4c7a5cc346ef27
Your Name <hoanganhdo182@gmail.com>
2025-07-03 15:11:34 +0700

    Fix: export handler using exports.handler for Netlify

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,6 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('./server/app');
 
-// Export handler trực tiếp
-const handler = serverless(app);
-module.exports = { handler }; 
+exports.handler = serverless(app); 
